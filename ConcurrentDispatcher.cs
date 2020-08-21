2af4118a94a4bb49355140621709c7fe58fce033
Alexey Savchenko <68587838+goOrn@users.noreply.github.com>
2020-08-21 18:02:37 +0300

    Update ConcurrentDispatcher.cs

--- ConcurrentDispatcher.cs
+++ ConcurrentDispatcher.cs
@@ -3,7 +3,7 @@ using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace pipeline_web_client
+namespace Default
 {
     public class ConcurrentDispatcher<T>
     {
