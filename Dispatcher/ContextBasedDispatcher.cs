de369caba73d17abe56d61dddea91f456e23ec38
Alexey Savchenko <68587838+goOrn@users.noreply.github.com>
2020-08-24 18:31:38 +0300

    Add files via upload

---
+++ Dispatcher/ContextBasedDispatcher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace pipeline_client
+{
+    public class ContextBasedDispatcher<TContext, TItem>
+    {
+        private readonly ConcurrentDictionary<TContext, ConcurrentDispatcher<TItem>> _map
+            = new ConcurrentDictionary<TContext, ConcurrentDispatcher<TItem>>();
+
+        public void Dispatch(TContext context, TItem item, Func<TItem, Task> handler)
+        {
+            if (!_map.TryGetValue(context, out var dispatcher))
+            {
+                var newDispatcher = new ConcurrentDispatcher<TItem>();
+                dispatcher = _map.GetOrAdd(context, newDispatcher);
+            }
+            
+            dispatcher.Dispatch(item, handler);
+        }
+    }
+}
